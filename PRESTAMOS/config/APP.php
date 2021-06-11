8f2b64c5e70e17f9f25685fc81a2bd8387024ee8
EMBenavidez2020 <40631518+EMBenavidez2020@users.noreply.github.com>
2021-06-11 00:23:11 -0300

    Add files via upload
    Primera entrega del proyecto

---
+++ PRESTAMOS/config/APP.php
@@ -0,0 +1,5 @@
+<?php
+    const SERVERURL="http://127.0.0.1/prestamos/";
+    const COMPANY="SISTEMAS DE PRESTAMOS";
+    const MONEDA="$";
+    date_default_timezone_set("America/Argentina/La_Rioja");
